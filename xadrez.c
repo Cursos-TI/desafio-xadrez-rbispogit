b0da151d7338ba3d630e399b7f77f0838b605193
Robson <rbispo.eng@gmail.com>
2025-08-30 20:02:12 +0000

    Criei a estrutura do desafio aventureiro

--- xadrez.c
+++ xadrez.c
@@ -4,7 +4,8 @@
 // Este código inicial serve como base para o desenvolvimento do sistema de movimentação das peças de xadrez.
 // O objetivo é utilizar estruturas de repetição e funções para determinar os limites de movimentação dentro do jogo.
 
-int main() {
+int main()
+{
     // Nível Novato - Movimentação das Peças
     // Sugestão: Declare variáveis constantes para representar o número de casas que cada peça pode se mover.
 
@@ -29,39 +30,40 @@ int main() {
     // Inclua o uso de continue e break dentro dos loops.
 
     int torre = 1;
-    int bispo = 0; 
+    int bispo = 0;
     int rainha = 1;
-    int casas;
+    int movimentoCompleto = 1;
 
-    printf("Andar quantas casas com a torre? ");
-    scanf("%d", &casas);
-
-    while (torre <= casas)
+    while (torre <= 5)
     {
         printf("Direita\n");
         torre++;
     }
-
-    printf("Andar quantas casas com o bispo? ");
-    scanf("%d", &casas);
 
     do
     {
         bispo++;
         printf("Cima\n");
         printf("Direita\n");
-    } while (bispo < casas);
+    } while (bispo < 3);
 
-    printf("Andar quantas casas com a rainha? ");
-    scanf("%d", &casas);
-
-    for (int i = 0; i < casas; i++)
+    for (int i = 0; i < 2; i++)
     {
         printf("Esquerda\n");
     }
-    
-    
-    
+
+    printf("\n");
+
+    while (movimentoCompleto--)
+    {
+
+        for (int i = 0; i < 2; i++)
+        {
+            printf("Baixo\n");
+        }
+
+        printf("Esquerda.\n");
+    }
 
     return 0;
 }
